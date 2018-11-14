8a6a555e3b11e04f8e7a512b605bee32474f7ab3
Kyle.Hoobler.15 <kyle.hoobler.15@cnu.edu>
2018-11-14 13:34:09 -0500

    Made a few classes and modified graph interface

--- app/src/main/java/kylehoobler/agc/GraphView.java
+++ app/src/main/java/kylehoobler/agc/GraphView.java
@@ -4,12 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+
 public class GraphView extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private GraphAdapter adapter;
+    private ArrayList equations;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
@@ -26,9 +35,32 @@ public class GraphView extends AppCompatActivity {
         Toolbar myToolBar = findViewById(R.id.GraphBar);
         setSupportActionBar(myToolBar);
 
+        recyclerView = findViewById(R.id.graphList);
+        equations = new ArrayList<Equation>();
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+
+        recyclerView.setLayoutManager(layoutManager);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), layoutManager.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
+        Equation test = new Equation();
+        addABunch();
+
+        adapter = new GraphAdapter(this, equations);
+        recyclerView.setAdapter(adapter);
+
+        recyclerView.setNestedScrollingEnabled(false);
 
 
 
+    }
+
+    private void addABunch(){
+        for(int i = 0; i < 100; i++){
+            equations.add(new Equation());
+        }
     }
 
     @Override
